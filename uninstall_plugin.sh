2a155b596e751c7193ac8e5206eb00b0123c98fb
Nathan Schagen <nschagen@gmail.com>
2013-07-06 12:45:35 +0200

    Added unfinished plugin uninstaller

---
+++ uninstall_plugin.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]; then
+  echo "please specify the name of a plugin!" >&2
+  exit
+fi
+
+if [ -d ".git" ]; then
+  #Uninstall all given plugins
+  while [ "$1" != "" ]; do
+    plugin=$1
+
+    if [ -d "bundle/$plugin" ]; then
+      echo "Uninstalling plugin $plugin"
+      echo "git submodule deinit bundle/$plugin"
+      #TODO: remove it from .gitmodules!!!
+      echo "git rm --cached bundle/$plugin"
+    else
+      echo "Plugin $plugin does not exists. Skipping!"
+    fi
+    shift
+  done
+else
+  echo "You're not in the top level directory!"
+fi
